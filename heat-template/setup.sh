89cf5a08261e0f60dfc8c871e777ce608ddbaacf
mtangaro <ma.tangaro@gmail.com>
2016-04-27 13:07:39 +0000

    fix ansible roles

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -5,5 +5,5 @@
 yum install -y epel-release
 yum install -y ansible
 
-git clone https://github.com/mtangaro/ansible-role-galaxycloud.git /tmp/ansible-role-galaxycloud
-cp -r /tmp/ansible-role-galaxycloud/* /etc/ansible
+git clone https://github.com/mtangaro/ansible-role-galaxycloud.git /tmp/galaxycloud
+cp -r /tmp/galaxycloud /etc/ansible/roles/
