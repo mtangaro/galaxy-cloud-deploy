7e838153603db466eac6c14f2370854b633b09f5
mtangaro <ma.tangaro@gmail.com>
2016-04-29 08:40:01 +0000

    enable ansible log

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -7,3 +7,6 @@ yum install -y ansible
 
 git clone https://github.com/mtangaro/ansible-role-galaxycloud.git /tmp/galaxycloud
 cp -r /tmp/galaxycloud /etc/ansible/roles/
+
+#enable ansible log file
+sed -i 's\^#log_path = /var/log/ansible.log.*$\log_path = /var/log/ansible.log\' /etc/ansible/ansible.cfg
