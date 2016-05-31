67c192e4c81c2f22bc872640fdb5f21f8d3971bc
mtangaro <ma.tangaro@gmail.com>
2016-05-31 10:19:19 +0000

    add setuptools.sh

---
+++ heat-template/setuptools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# copy ansible roles
+
+yum install -y epel-release
+yum install -y ansible
+
+git clone https://github.com/mtangaro/ansible-role-galaxycloud.git /tmp/galaxycloud
+cd /tmp/galaxycloud && git checkout devel
+cp -r /tmp/galaxycloud /etc/ansible/roles/
+
+#enable ansible log file
+sed -i 's\^#log_path = /var/log/ansible.log.*$\log_path = /var/log/ansible.log\' /etc/ansible/ansible.cfg
