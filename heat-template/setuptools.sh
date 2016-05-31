91afc79cb1f9caf1ef205dc8dfa700473398b10a
mtangaro <ma.tangaro@gmail.com>
2016-05-31 10:21:21 +0000

    add official galaxyproject ansible-galaxy-tools

--- heat-template/setuptools.sh
+++ heat-template/setuptools.sh
@@ -9,5 +9,8 @@ git clone https://github.com/mtangaro/ansible-role-galaxycloud.git /tmp/galaxycl
 cd /tmp/galaxycloud && git checkout devel
 cp -r /tmp/galaxycloud /etc/ansible/roles/
 
+git clone https://github.com/galaxyproject/ansible-galaxy-tools.git /tmp/galaxy-tools
+cp -r /tmp/galaxy-tools /etc/ansible/roles/
+
 #enable ansible log file
 sed -i 's\^#log_path = /var/log/ansible.log.*$\log_path = /var/log/ansible.log\' /etc/ansible/ansible.cfg
