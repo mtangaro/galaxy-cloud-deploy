10a78fd7984ef57106636d63c4f31aa45a1b35aa
mtangaro <ma.tangaro@gmail.com>
2016-06-23 14:39:09 +0000

    add tools install

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -14,8 +14,12 @@ yum install -y ansible git
 
 # Install ansible-role-galaxycloud
 git clone https://github.com/mtangaro/ansible-role-galaxycloud.git /tmp/galaxycloud
-cd /tmp/galaxycloud && git checkout master
+cd /tmp/galaxycloud && git checkout devel
 cp -r /tmp/galaxycloud /etc/ansible/roles/
+
+# Install galaxy-tool playbook and  galaxyproject/ansible-galaxy-tools
+git clone --recursive https://github.com/mtangaro/galaxycloud-tools-playbook.git /tmp/galaxycloud-tools
+cp -r /tmp/galaxycloud-tools/* /etc/ansible/
 
 # Enable ansible log file
 sed -i 's\^#log_path = /var/log/ansible.log.*$\log_path = /var/log/ansible.log\' /etc/ansible/ansible.cfg
