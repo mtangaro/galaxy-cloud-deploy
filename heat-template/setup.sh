d8ee3eb56d9e9dddd91d31385c915363e66c9ed0
mtangaro <ma.tangaro@gmail.com>
2016-06-25 17:16:02 +0000

    enabled and fixed external volumes

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 #######################################
+# Mount external volumes
+#######################################
+
+#Allow user to use UserData volume
+voldata_id=$userdata_volid
+voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
+mkdir -p $userdata_mountpoint
+mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
+echo "Successfully device mounted (working dir)"
+mkdir -p $userdata_mountpoint/galaxy
+chown -R galaxy:galaxy $userdata_mountpoint/galaxy
+
+##Allow user to use ReferenceData volume
+ref_voldata_id=$refdata_volid
+ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
+mkdir -p $refdata_mountpoint
+mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
+echo "Successfully device mounted (reference data)"
+mkdir -p $refdata_mountpoint/galaxy
+chown -R galaxy:galaxy $refdata_mountpoint/galaxy
+
+
+#######################################
 # Copy ansible roles
 #
-# This script install Ansible and copy to /etc/ansible/roles
+# This section install Ansible and copy to /etc/ansible/roles
 # the ansible-role-galaxycloud and related playbooks
 #######################################
 
