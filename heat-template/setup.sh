121e2e6581383a9866c3db7aba78669c40f647da
mtangaro <ma.tangaro@gmail.com>
2016-07-26 10:23:59 +0000

    update

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -4,23 +4,23 @@
 # Mount external volumes
 #######################################
 
-##Allow user to use User-Data volume
-#voldata_id=$userdata_volid
-#voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
-#mkdir -p $userdata_mountpoint
-#mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
-#echo "Successfully device mounted (working dir)"
-#mkdir -p $userdata_mountpoint/galaxy
-#chown -R galaxy:galaxy $userdata_mountpoint/galaxy
+#Allow user to use User-Data volume
+voldata_id=$userdata_volid
+voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
+mkdir -p $userdata_mountpoint
+mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
+echo "Successfully device mounted (working dir)"
+mkdir -p $userdata_mountpoint/galaxy
+chown -R galaxy:galaxy $userdata_mountpoint/galaxy
 
-##Allow user to use Reference-Data volume
-#ref_voldata_id=$refdata_volid
-#ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
-#mkdir -p $refdata_mountpoint
-#mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
-#echo "Successfully device mounted (reference data)"
-#mkdir -p $refdata_mountpoint/galaxy
-#chown -R galaxy:galaxy $refdata_mountpoint/galaxy
+#Allow user to use Reference-Data volume
+ref_voldata_id=$refdata_volid
+ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
+mkdir -p $refdata_mountpoint
+mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
+echo "Successfully device mounted (reference data)"
+mkdir -p $refdata_mountpoint/galaxy
+chown -R galaxy:galaxy $refdata_mountpoint/galaxy
 
 
 #######################################
@@ -39,10 +39,6 @@ yum install -y ansible git
 git clone https://github.com/indigo-dc/ansible-role-galaxycloud.git /tmp/galaxycloud
 cd /tmp/galaxycloud && git checkout master
 cp -r /tmp/galaxycloud /etc/ansible/roles/
-
-# Install galaxy-tool playbook and  galaxyproject/ansible-galaxy-tools
-git clone --recursive https://github.com/mtangaro/galaxycloud-tools-playbook.git /tmp/galaxycloud-tools
-cp -r /tmp/galaxycloud-tools/* /etc/ansible/
 
 # Enable ansible log file
 sed -i 's\^#log_path = /var/log/ansible.log.*$\log_path = /var/log/ansible.log\' /etc/ansible/ansible.cfg
