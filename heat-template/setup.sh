b699f03304a8e9ed28b74ca360d00293aeac398d
mtangaro <ma.tangaro@gmail.com>
2016-07-29 11:20:20 +0000

    update playbook, comment exernal drive

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -5,22 +5,22 @@
 #######################################
 
 #Allow user to use User-Data volume
-voldata_id=$userdata_volid
-voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
-mkdir -p $userdata_mountpoint
-mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
-echo "Successfully device mounted (working dir)"
-mkdir -p $userdata_mountpoint/galaxy
-chown -R galaxy:galaxy $userdata_mountpoint/galaxy
+#voldata_id=$userdata_volid
+#voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
+#mkdir -p $userdata_mountpoint
+#mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
+#echo "Successfully device mounted (working dir)"
+#mkdir -p $userdata_mountpoint/galaxy
+#chown -R galaxy:galaxy $userdata_mountpoint/galaxy
 
 #Allow user to use Reference-Data volume
-ref_voldata_id=$refdata_volid
-ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
-mkdir -p $refdata_mountpoint
-mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
-echo "Successfully device mounted (reference data)"
-mkdir -p $refdata_mountpoint/galaxy
-chown -R galaxy:galaxy $refdata_mountpoint/galaxy
+#ref_voldata_id=$refdata_volid
+#ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
+#mkdir -p $refdata_mountpoint
+#mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
+#echo "Successfully device mounted (reference data)"
+#mkdir -p $refdata_mountpoint/galaxy
+#chown -R galaxy:galaxy $refdata_mountpoint/galaxy
 
 
 #######################################
@@ -37,7 +37,7 @@ yum install -y ansible git
 
 # Install ansible-role-galaxycloud
 git clone https://github.com/indigo-dc/ansible-role-galaxycloud.git /tmp/galaxycloud
-cd /tmp/galaxycloud && git checkout master
+cd /tmp/galaxycloud && git checkout devel
 cp -r /tmp/galaxycloud /etc/ansible/roles/
 
 # Enable ansible log file
