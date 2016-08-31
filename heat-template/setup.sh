647d013c00e4211de8153bed0dae14318d3f7a84
mtangaro <ma.tangaro@gmail.com>
2016-08-31 18:26:56 +0000

    update centos

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -4,24 +4,23 @@
 # Mount external volumes
 #######################################
 
-#Allow user to use User-Data volume
-#voldata_id=$userdata_volid
-#voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
-#mkdir -p $userdata_mountpoint
-#mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
-#echo "Successfully device mounted (working dir)"
-#mkdir -p $userdata_mountpoint/galaxy
-#chown -R galaxy:galaxy $userdata_mountpoint/galaxy
+#---
+# Allow user to use User-Data volume
 
-#Allow user to use Reference-Data volume
-#ref_voldata_id=$refdata_volid
-#ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
-#mkdir -p $refdata_mountpoint
-#mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
-#echo "Successfully device mounted (reference data)"
-#mkdir -p $refdata_mountpoint/galaxy
-#chown -R galaxy:galaxy $refdata_mountpoint/galaxy
+voldata_id=$userdata_volid
+voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
+mkdir -p $userdata_mountpoint
+mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
+echo "Successfully device mounted (working dir)"
 
+#---
+# Allow user to use Reference-Data volume
+
+ref_voldata_id=$refdata_volid
+ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
+mkdir -p $refdata_mountpoint
+mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
+echo "Successfully device mounted (reference data)"
 
 #######################################
 # Copy ansible roles
@@ -42,4 +41,3 @@ cp -r /tmp/galaxycloud /etc/ansible/roles/
 
 # Enable ansible log file
 sed -i 's\^#log_path = /var/log/ansible.log.*$\log_path = /var/log/ansible.log\' /etc/ansible/ansible.cfg
-
