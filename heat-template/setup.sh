d218dbb740474245dc738eafaa6feb34e0c8c364
mtangaro <ma.tangaro@gmail.com>
2017-06-05 11:14:29 +0000

    install galaxy only

--- heat-template/setup.sh
+++ heat-template/setup.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
-#######################################
-# Mount external volumes
-#######################################
+GALAXY=true
+BRANCH="master"
 
-#---
-# Allow user to use User-Data volume
+TOOLS=true
+TOOLS_BRANCH="master"
+#TOOLS_BRANCH="handler-include-fix"
+#TOOLS_BRANCH="handler-include-static-no"
 
-voldata_id=$userdata_volid
-voldata_dev="/dev/disk/by-id/virtio-$(echo ${voldata_id} | cut -c -20)"
-mkdir -p $userdata_mountpoint
-mkfs.ext4 ${voldata_dev} && mount ${voldata_dev} $userdata_mountpoint || notify_err "Some problems occurred with block device (working dir)"
-echo "Successfully device mounted (working dir)"
-
-#---
-# Allow user to use Reference-Data volume
-
-#ref_voldata_id=$refdata_volid
-#ref_voldata_dev="/dev/disk/by-id/virtio-$(echo ${ref_voldata_id} | cut -c -20)"
-#mkdir -p $refdata_mountpoint
-#mkfs.ext4 ${ref_voldata_dev} && mount ${ref_voldata_dev} $refdata_mountpoint || notify_err "Some problems occurred with block device (reference data)"
-#echo "Successfully device mounted (reference data)"
-
-#######################################
-# Copy ansible roles
-#
-# This section install Ansible and copy to /etc/ansible/roles
-# the ansible-role-galaxycloud and related playbooks
-#######################################
-
-
+#______________________________________
 # Install Ansible
 
 LOGFILE="/tmp/setup.log"
@@ -61,44 +40,17 @@ sed -i 's\^#local_tmp      = ~/.ansible/tmp.*$\local_tmp      = $HOME/.ansible/t
 # Enable ansible log file
 sed -i 's\^#log_path = /var/log/ansible.log.*$\log_path = /var/log/ansible.log\' /etc/ansible/ansible.cfg
 
-#
+#______________________________________
 # Install Ansible roles
-#
 
-OS=true
-GALAXY=true
-TOOLS=true
-REFDATA=true
-
-###
-# 1. Install ansible-role-galaxycloud-os
-if $OS; then
-  git clone https://github.com/mtangaro/ansible-role-galaxycloud-os.git /tmp/galaxycloud-os &>> $LOGFILE
-  cd /tmp/galaxycloud-os && git checkout master &>> $LOGFILE
-  cp -r /tmp/galaxycloud-os /etc/ansible/roles/
-fi
-
-###
-# 2. Install ansible-role-galaxycloud
-BRANCH="devel"
+# Install ansible-role-galaxycloud
 if $GALAXY; then
-  git clone https://github.com/indigo-dc/ansible-role-galaxycloud.git /tmp/galaxycloud &>> $LOGFILE
-  cd /tmp/galaxycloud && git checkout $BRANCH &>> $LOGFILE
-  cp -r /tmp/galaxycloud /etc/ansible/roles/
+  git clone https://github.com/mtangaro/ansible-role-galaxycloud-galaxy-install.git /etc/ansible/roles/galaxy-install &>> $LOGFILE
+  cd /etc/ansible/roles/galaxy-install && git checkout $BRANCH &>> $LOGFILE
 fi
 
-###
-# 3. Install ansible-galaxy-tools
+# Install ansible-galaxy-tools
 if $TOOLS; then
-  git clone https://github.com/indigo-dc/ansible-galaxy-tools.git /tmp/galaxy-tools &>> $LOGFILE
-  cd /tmp/galaxy-tools && git checkout master &>> $LOGFILE
-  cp -r /tmp/galaxy-tools /etc/ansible/roles/
-fi
-
-###
-# 4. Install ansible-role-galaxycloud-refdata
-if $REFDATA; then
-  git clone https://github.com/mtangaro/ansible-role-galaxycloud-refdata.git /tmp/galaxycloud-refdata &>> $LOGFILE
-  cd /tmp/galaxycloud-refdata && git checkout master &>> $LOGFILE
-  cp -r /tmp/galaxycloud-refdata /etc/ansible/roles/
+  git clone https://github.com/indigo-dc/ansible-galaxy-tools.git /etc/ansible/roles/indigo-dc.galaxy-tools &>> $LOGFILE
+  cd /etc/ansible/roles/indigo-dc.galaxy-tools && git checkout $TOOLS_BRANCH &>> $LOGFILE
 fi
